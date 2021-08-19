413635b893ae8aca7614a6e4fe76d00051400f25
Eshita28 <73231235+Eshita28@users.noreply.github.com>
2021-08-19 18:05:09 +0530

    updated by sanjay

---
+++ js/first.js
@@ -0,0 +1,14 @@
+alert("first");
+var x=8;
+switch(x)
+{
+    default:
+        console.log("default")
+    case 1:
+        console.log("hello");
+    case 2:
+        console.log("hii");
+    case 3:
+        console.log("kkk")
+} 
+
